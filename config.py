3fa84565c888bae231f4b91077b0378978165369
kybrott-glitch <kybrott@gmail.com>
2026-06-08 13:09:50 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+from typing import List
+
+BOT_TOKEN: str = "8914328372:AAHloouLWY2Ld3JSMkj__Vjo2vLWifcT6zE"
+
+ADMIN_USER_IDS: List[int] = [
+    8765896690,  # ← your Telegram user ID
+]
+
+DATABASE_URL: str = "sqlite+aiosqlite:///./guestbot.db"
+
+INLINE_CACHE_TIME: int = 30
